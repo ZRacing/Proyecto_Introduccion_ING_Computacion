1f5c530fb2a6a6a7d0dd1a12919af1ae46912793
Gabriel <gabriel.hdez.530@gmail.com>
2024-11-25 06:27:21 -0600

    Update Corrector.cpp

--- Corrector.cpp
+++ Corrector.cpp
@@ -13,14 +13,7 @@
 #include "stdafx.h"
 #include <string.h>
 #include "corrector.h"
-//Funciones publicas del proyecto
-/*****************************************************************************************************************
-	DICCIONARIO: Esta funcion crea el diccionario completo
-	char *	szNombre				:	Nombre del archivo de donde se sacaran las palabras del diccionario
-	char	szPalabras[][TAMTOKEN]	:	Arreglo con las palabras completas del diccionario
-	int		iEstadisticas[]			:	Arreglo con el numero de veces que aparecen las palabras en el diccionario
-	int &	iNumElementos			:	Numero de elementos en el diccionario
-******************************************************************************************************************/
+
 void Diccionario(char* szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[], int& iNumElementos)
 {
 	FILE* fp = nullptr; // Puntero al archivo
