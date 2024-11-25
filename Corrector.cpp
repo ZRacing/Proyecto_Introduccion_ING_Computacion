a2deb027d9f7e2e2ce0c3775b80a6a84acee5e6b
Gabriel <gabriel.hdez.530@gmail.com>
2024-11-25 06:27:05 -0600

    Update Corrector.cpp

--- Corrector.cpp
+++ Corrector.cpp
@@ -72,18 +72,7 @@ void Diccionario(char* szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[
 
 }
 
-/*****************************************************************************************************************
-	ListaCandidatas: Esta funcion recupera desde el diccionario las palabras validas y su peso
-	Regresa las palabras ordenadas por su peso
-	char	szPalabrasSugeridas[][TAMTOKEN],	//Lista de palabras clonadas
-	int		iNumSugeridas,						//Lista de palabras clonadas
-	char	szPalabras[][TAMTOKEN],				//Lista de palabras del diccionario
-	int		iEstadisticas[],					//Lista de las frecuencias de las palabras
-	int		iNumElementos,						//Numero de elementos en el diccionario
-	char	szListaFinal[][TAMTOKEN],			//Lista final de palabras a sugerir
-	int		iPeso[],							//Peso de las palabras en la lista final
-	int &	iNumLista)							//Numero de elementos en la szListaFinal
-******************************************************************************************************************/
+
 void ListaCandidatas(
 	char szPalabrasSugeridas[][TAMTOKEN],
 	int iNumSugeridas,
